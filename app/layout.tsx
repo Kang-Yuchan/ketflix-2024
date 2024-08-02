bf61403d124673727bef00772e1e3b476ff735fd
Kang-Yuchan <mikuplayer123@gmail.com>
2024-08-03 02:36:51 +0900

    feat: add home landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Header />
-          <main>{children}</main>
+          <main className="flex-1">{children}</main>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
 'use client';
 
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+
 export default function Home() {
-  return <div className="">content</div>;
+  const { theme } = useTheme();
+  const isDarkMode = theme === 'dark';
+
+  return (
+    <div className="w-full py-12 md:py-24 lg:py-32">
+      <div className="container px-4 md:px-6">
+        <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
+          <div className="flex flex-col justify-center space-y-4">
+            <div
+              className={`space-y-2 ${isDarkMode ? 'text-white' : 'text-black'} `}
+            >
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                Yuchan Kang
+              </h1>
+              <h2 className="text-xl font-medium">Front-End Developer</h2>
+              <p className="max-w-[600px] md:text-xl">
+                自信、好奇心、謙遜を忘れないエンジニア
+              </p>
+            </div>
+          </div>
+          <Image
+            src="/avatar.jpg"
+            width="550"
+            height="550"
+            alt="Avatar"
+            className="mx-auto aspect-square overflow-hidden rounded-xl object-cover sm:w-full lg:order-last"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
