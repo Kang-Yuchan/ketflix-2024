87a4c1dcd4bc4996adde4c6538372a6b7e3bd5b8
Kang-Yuchan <mikuplayer123@gmail.com>
2024-08-04 17:15:07 +0900

    feat: add isLoading state in useProjects hook

--- hooks/useProjects.ts
+++ hooks/useProjects.ts
@@ -7,10 +7,11 @@ const fetcher = (url: string) =>
   fetch(url).then((res) => res.json() as Promise<Project[]>);
 
 const useProjects = () => {
-  const { data: projects, error } = useSWR('/api/projects', fetcher);
+  const { data: projects, error, isLoading } = useSWR('/api/projects', fetcher);
   return {
     projects,
     error,
+    isLoading,
   };
 };
 export default useProjects;
